604a54d42959d360a47c3114cd22fa4e390bf823
Matin-Ardestani <m.matin.ardestani@gmail.com>
2021-08-24 23:46:38 +0430

    full app with a bad UI

--- src/main.py
+++ src/main.py
@@ -1,23 +1,45 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+import sys
 
-from typingtest import Ui_TestWindow
-from welcome import Ui_WelcomeWindow
+from pages import Ui_PagesWindow
 
-
-class Welcome(QMainWindow):
+class RootMain(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
-
-        self.ui = Ui_WelcomeWindow()
+        self.ui = Ui_PagesWindow()
         self.ui.setupUi(self)
 
+        self.ui.btn_test.clicked.connect(lambda: [self.ui.pages.setCurrentWidget(self.ui.page_test) , self.ui.btn_test.setStyleSheet('background: rgb(106, 151, 159); border-radius: 2px;'),
+        self.ui.btn_setting.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_about.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_help.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;')
+        ])
+
+        self.ui.btn_setting.clicked.connect(lambda: [self.ui.pages.setCurrentWidget(self.ui.page_setting) , self.ui.btn_setting.setStyleSheet('background: rgb(106, 151, 159); border-radius: 2px;'),
+        self.ui.btn_test.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_about.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_help.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;')
+        ])
+
+        self.ui.btn_help.clicked.connect(lambda: [self.ui.pages.setCurrentWidget(self.ui.page_help) , self.ui.btn_help.setStyleSheet('background: rgb(106, 151, 159); border-radius: 2px;'),
+        self.ui.btn_test.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_setting.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_about.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        ])
+
+        self.ui.btn_about.clicked.connect(lambda: [self.ui.pages.setCurrentWidget(self.ui.page_about) , self.ui.btn_about.setStyleSheet('background: rgb(106, 151, 159); border-radius: 2px;'),
+        self.ui.btn_test.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_setting.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        self.ui.btn_help.setStyleSheet('background:rgb(211, 221, 221); border-radius: 2px;'),
+        ])
+
+        # removing borders
         self.setWindowFlag(Qt.FramelessWindowHint)
         self.setAttribute(Qt.WA_TranslucentBackground)
 
         self.show()
 
-        self.ui.pushButton.clicked.connect(self.main)
     def main(self):
         self.mainwindow = RootMain()
         self.mainwindow.show()
@@ -31,27 +53,7 @@ class Welcome(QMainWindow):
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.oldPos = evt.globalPos()
 
-class RootMain(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
-
-        self.ui = Ui_TestWindow()
-        self.ui.setupUi(self)
-
-        self.setWindowFlag(Qt.FramelessWindowHint)
-        self.setAttribute(Qt.WA_TranslucentBackground)
-
-
-    def mousePressEvent(self , evt):
-        self.oldPos = evt.globalPos()
-
-    def mouseMoveEvent(self , evt):
-        delta = QPoint(evt.globalPos() - self.oldPos)
-        self.move(self.x() + delta.x(), self.y() + delta.y())
-        self.oldPos = evt.globalPos()
-
 if __name__ == '__main__':
-    import sys
     app = QApplication(sys.argv)
-    root = Welcome()
+    root = RootMain()
     sys.exit(app.exec_())
